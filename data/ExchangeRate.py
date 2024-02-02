25dd9a2663b7f7d19c40e76958dfa18d7e8c43eb
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2024-02-02 22:39:18 +0100

    Edycja Frontu Aplikacji, przygotowanie do kreacji API

--- data/ExchangeRate.py
+++ data/ExchangeRate.py
@@ -2,19 +2,20 @@ from forex_python.converter import CurrencyRates
 from datetime import datetime, timedelta
 
 
-# def actual_ron():
+# def actual_ron(currency_code):
 #     c = CurrencyRates()
-#     kurs_ron = c.get_rate('RON', 'USD')  # Możesz zmienić 'USD' na inną walutę według potrzeb
-#     print(f'Actual RON: 1 RON = {kurs_ron:.4f} USD')
+#     rate_ron = c.get_rate('RON', currency_code)
+#     print(f'Actual RON: 1 RON = {rate_ron:.4f}' currency_code)
 
 
-def exchange_rate_ron(start_date, end_date):
+def exchange_rate_ron(start_date, end_date, currency_code):
     c = CurrencyRates()
 
     current_date = start_date
     while current_date <= end_date:
-        rate_ron = c.get_rate('RON', 'PLN', date_obj=current_date)  # Możesz zmienić 'PLN' na inną walutę według potrzeb
-        print(f'exchange rate RON for {current_date.date()}: 1 RON = {rate_ron:.4f} PLN')
+        rate_ron = c.get_rate('RON', currency_code, date_obj=current_date)  # currency code to kod waluty. Np. PLN
+        # print(f'exchange rate RON for {current_date.date()}: 1 RON = {rate_ron:.4f} ' + currency_code)
+        print(f'{rate_ron:.4f} {currency_code}')
         current_date += timedelta(days=1)
 
 
@@ -25,4 +26,7 @@ if __name__ == "__main__":
     # Archiwalne kursy RON (przykład dla ostatnich 7 dni)
     start_date = datetime.now() - timedelta(days=7)
     end_date = datetime.now()
-    exchange_rate_ron(start_date, end_date)
+
+    # exchange_rate_ron(start_date, end_date, 'PLN')
+    # exchange_rate_ron(start_date, end_date, 'USD')
+    # exchange_rate_ron(start_date, end_date, 'CZK')
--- data/data_manager.py
+++ data/data_manager.py
@@ -5,12 +5,18 @@ conn = sqlite3.connect("C:\\Users\\Eteer\\Desktop\\AlfaForwading\\AlfaForwarding
 cur = conn.cursor()
 
 cur.execute('''
-    CREATE TABLE IF NOT EXISTS RonExchangeRate (record_id INTEGER PRIMARY KEY AUTOINCREMENT,currency_code VARCHAR(3), rate_value DECIMAL(10,4))
+    CREATE TABLE IF NOT EXISTS RonExchangeRate (
+        record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        currency_code VARCHAR(3),
+        rate_value DECIMAL(10,4),
+        country_name VARCHAR(100),
+        date_time DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
 ''')
 
-# cur.execute('''
-#     INSERT INTO RonExchangeRate (currency_code,rate_value) VALUES("PWM",2.25)
-# ''')
+cur.execute('''
+    INSERT INTO RonExchangeRate (currency_code,rate_value) VALUES("PWM",2.25)
+''')
 conn.commit()
 
 cur.execute('''
