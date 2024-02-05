113219e5ae80ea1189796f07918d3ba6ea229f82
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2024-02-05 09:23:58 +0100

    Rozbudowa Data_manager.py

--- data/data_manager.py
+++ data/data_manager.py
@@ -1,30 +1,42 @@
 import sqlite3
 
 conn = sqlite3.connect("C:\\Users\\Eteer\\Desktop\\AlfaForwading\\AlfaForwardingTest\\database\\ExchangeRate.db")
-
 cur = conn.cursor()
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS RonExchangeRate (
-        record_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        currency_code VARCHAR(3),
-        rate_value DECIMAL(10,4),
-        country_name VARCHAR(100),
-        date_time DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-''')
 
-cur.execute('''
-    INSERT INTO RonExchangeRate (currency_code,rate_value) VALUES("PWM",2.25)
-''')
-conn.commit()
+def create_table():
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS RonExchangeRate (
+            record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            currency_code VARCHAR(3),
+            currency_name VARCHAR(100),
+            rate_value DECIMAL(10,4),
+            date_time DATETIME
+        )
+    ''')
 
-cur.execute('''
-    SELECT * FROM RonExchangeRate
-''')
 
-for data in cur:
-    print(data)
+def insert_data(currency_code, currency_name, rate_value, date_time):
+    cur.execute('''
+        INSERT INTO RonExchangeRate (currency_code,currency_name, rate_value,date_time) VALUES (?, ?, ?, ?)
+    ''', (currency_code, currency_name, rate_value, date_time))
+    conn.commit()
 
-cur.close()
-conn.close()
+
+def fetch_data():
+    cur.execute('''
+        SELECT * FROM RonExchangeRate
+    ''')
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+
+
+def close_connection():
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    fetch_data()
+    close_connection()
